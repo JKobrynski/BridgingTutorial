874e12b7fda808fe4083e5bb145f94574347121f
Julian Kobrynski <julian.kobrynski@callstack.com>
2024-01-24 19:05:29 +0100

    implement file picker module

--- App.tsx
+++ App.tsx
@@ -1,117 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {Button, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {SaveFilePickerModule} from 'save-file-picker-package';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const ASSETS_FILE_PATH = 'file.html';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  const saveFileWithCallback = () => {
+    SaveFilePickerModule.saveFileWithCallback(ASSETS_FILE_PATH, result => {
+      console.log(result);
+    });
+  };
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const saveFileWithPromise = async () => {
+    try {
+      const isSuccess = await SaveFilePickerModule.saveFileWithPromise(
+        ASSETS_FILE_PATH,
+      );
+
+      console.log({isSuccess});
+    } catch (error) {
+      console.log({error});
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.header}>Save file picker</Text>
+        <View style={styles.body}>
+          <View style={styles.buttonWrapper}>
+            <Button
+              onPress={saveFileWithCallback}
+              title="Save file (Callback)"
+            />
+          </View>
+          <View style={styles.buttonWrapper}>
+            <Button onPress={saveFileWithPromise} title="Save file (Promise)" />
+          </View>
         </View>
-      </ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  body: {
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 20,
   },
-  sectionTitle: {
+  buttonWrapper: {
+    alignSelf: 'stretch',
+    paddingVertical: 30,
+  },
+  container: {
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    flex: 1,
+    justifyContent: 'center',
+  },
+  header: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: 'bold',
+    paddingVertical: 20,
+    textTransform: 'capitalize',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  safeArea: {
+    alignSelf: 'stretch',
+    flex: 1,
   },
 });
 
---
+++ save-file-picker-package/ios/SaveFilePickerPackage-Bridging-Header.h
@@ -0,0 +1,2 @@
+
+#import <React/RCTUtils.h>
