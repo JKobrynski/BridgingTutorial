72a54562955ef3fe1e5f9d55c8c0dcc8da7422cd
Julian Kobrynski <julian.kobrynski@callstack.com>
2024-01-28 13:30:17 +0100

    view boilerplate

--- babel.config.js
+++ babel.config.js
@@ -20,6 +20,7 @@ module.exports = {
         alias: {
           'save-file-picker-package': './save-file-picker-package',
           'conic-gradient-package': './conic-gradient-package',
+          'range-slider-package': './range-slider-package',
         },
       },
     ],
